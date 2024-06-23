79dcb167215a7bffea35d110dfa7587a2d14ff46
SaZiKK <ck_look@outlook.com>
2024-06-23 21:53:04 +0800

    🐞 fix: alloc too much space to kernel heap

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,7 +7,7 @@ pub const USER_STACK_SIZE: usize = 4096 * 20;
 /// kernel stack size
 pub const KERNEL_STACK_SIZE: usize = 4096 * 8;
 /// kernel heap size
-pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0xc000;
+pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x500;
 /// physical memory end address
 pub const MEMORY_END: usize = 0x88000000;
 /// page size : 4KB
