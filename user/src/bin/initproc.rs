8f8cea4e84e8bf6d39ca1f142fa5ab7a99983412
Nelson Boss <bosswnx@outlook.com>
2024-07-30 04:10:44 +0800

    ✨ feat: 修改 initproc 执行 busybox sh

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -4,42 +4,32 @@
 extern crate user_lib;
 
 use user_lib::{exec, fork, wait, yield_, println};
-const ALL_TASKS: [&str; 1] = [
-    "time-test\0"
-];
 
 #[no_mangle]
 fn main() -> i32 {
-
     println!("[initproc] Start running...");
-
-    let mut app_num = 0;
-
-    for app in ALL_TASKS {
-        app_num += 1;
-        if fork() == 0 {
-            println!("[initproc] Running app: {}", app);
-            // 在子进程中执行应用程序
-            exec(app, &[core::ptr::null::<u8>()]);
-        }
-    }
-
+    let task = "busybox\0";
+    // q: 如果我想传递参数 sh 呢
+    let args = ["sh\0"];
+    let a = args.map(|arg| arg.as_ptr());
+    println!("[initproc] exec busybox sh...");
+    exec(&task, &a);
     // 父进程等待所有子进程结束
-    while app_num > 0 {
+    loop {
         let mut exit_code: i32 = 0;
         let pid = wait(&mut exit_code);
         if pid == -1 {
             yield_();
             continue;
         }
-        app_num -= 1;
         
         println!(
             "[initproc] Released a zombie process, pid={}, exit_code={}",
             pid,
             exit_code,
         );
-        
+
+        break;
     }
 
     0
