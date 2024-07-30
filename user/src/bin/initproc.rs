d45bd075aff155abeac8c5fb0e5e138e9b0d4ca5
SaZiKK <ck_look@outlook.com>
2024-07-31 02:21:19 +0800

    🐞 fix: fix initproc

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -8,29 +8,35 @@ use user_lib::{exec, fork, wait, yield_, println};
 #[no_mangle]
 fn main() -> i32 {
     println!("[initproc] Start running...");
-    let task = "busybox\0";
-    // q: 如果我想传递参数 sh 呢
-    let args = ["sh\0"];
-    let a = args.map(|arg| arg.as_ptr());
-    println!("[initproc] exec busybox sh...");
-    exec(&task, &a);
-    // 父进程等待所有子进程结束
-    loop {
-        let mut exit_code: i32 = 0;
-        let pid = wait(&mut exit_code);
-        if pid == -1 {
-            yield_();
-            continue;
+
+    if fork() == 0 {
+        let task = "busybox\0";
+        // q: 如果我想传递参数 sh 呢
+        let args = ["sh\0"];
+        let a = args.map(|arg| arg.as_ptr());
+        println!("[initproc] exec busybox sh...");
+        exec(&task, &a);
+    } else {
+        // 父进程等待所有子进程结束
+        loop {
+            let mut exit_code: i32 = 0;
+            let pid = wait(&mut exit_code);
+            if pid == -1 {
+                yield_();
+                continue;
+            }
+            
+            println!(
+                "[initproc] Released a zombie process, pid={}, exit_code={}",
+                pid,
+                exit_code,
+            );
+
+            if pid == -10 {
+                println!("[initproc] All tasks have exited, shutting down...");
+                break;
+            }
         }
-        
-        println!(
-            "[initproc] Released a zombie process, pid={}, exit_code={}",
-            pid,
-            exit_code,
-        );
-
-        break;
     }
-
     0
 }
